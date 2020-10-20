8b350207581ffcea9c15bab6363351aff823cab7
Petro Kok <petro.kok@otakoyi.com.ua>
2020-10-20 10:10:01 +0300

    Init

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,8 @@ router.get('/:user_id/toggle-access', userController.toggleUserAccess);
 
 router.get('/:user_id/parse', userController.parseFile);
 
+router.get('/test', function (req, res) {
+    console.log('STARTED GOOD!');
+});
+
 module.exports = router;
